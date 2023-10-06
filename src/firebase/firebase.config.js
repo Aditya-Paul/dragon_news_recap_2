cf2826cf1da4936c4ad7230533d89477620a3c73
Aditya paul <adityapaul.dibbo25@gmail.com>
2023-10-06 17:05:20 +0600

    this is

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
-
+// this is fire base config
 // Your web app's Firebase configuration
 console.log('inside firebase config',import.meta.env.VITE_PASS)
 const firebaseConfig = {
